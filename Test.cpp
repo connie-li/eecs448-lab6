098675cc7fc8d914d504e7884f72be469211a3cb
Connie Li <constanceli@ku.edu>
2019-11-11 13:35:18 -0600

    feat: add Test files

---
+++ Test.cpp
@@ -0,0 +1,8 @@
+/**
+ * @file Test.cpp
+ * @author Connie Li
+ * @date 2019-11-11
+ */
+
+#include "Test.h"
+using namespace std;
---
+++ Test.h
@@ -0,0 +1,19 @@
+/**
+ * @file Test.h
+ * @author Connie Li
+ * @date 2019-11-11
+ */
+
+#ifndef TEST_H
+#define TEST_H
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Test
+{
+
+};
+
+#endif
